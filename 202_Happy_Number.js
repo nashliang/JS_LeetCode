6e1d7558df9ab0dcaaa42f0dc375f7dd45449504
nashliang <nashliang13@gmail.com>
2017-04-25 22:17:50 +0800

    202 bug fix

--- 202_Happy_Number.js
+++ 202_Happy_Number.js
@@ -1,15 +1,15 @@
-function evaluate(element,index,array){
-	return Math.sqrt(element);
+function evaluate(sum,element){
+	return ( sum + Math.pow(element,2) );
 }
 
 function happyNumCheck(x){
 	var temp;
 	var mem={};
-	while(temp != 1 && !mem[x]){
+	while(temp != 1 && !mem[x]){   //20放在 mem[20]
 	 
-	    temp = x.toString().split('').reduce(evaluate);
+	    temp = x.toString().split('').reduce(evaluate,0);
 
-		mem[x]=temp;
+		mem[x] = temp;
 
 		x = temp;	
 		
@@ -17,3 +17,7 @@ function happyNumCheck(x){
 	return temp===1? true : false;
 }
 console.log(happyNumCheck(19));//true
+console.log(happyNumCheck(31));//true
+console.log(happyNumCheck(44));//true
+console.log(happyNumCheck(4));//false
+console.log(happyNumCheck(30));//false
